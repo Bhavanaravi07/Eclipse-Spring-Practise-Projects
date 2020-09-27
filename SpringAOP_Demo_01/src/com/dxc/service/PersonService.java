90857ffe60d3da6fabe1fc8fc068e17afe690ac0
bravichandr2 <bravichandr2@host.docker.internal>
2020-09-27 11:21:25 +0530

    commit

---
+++ SpringAOP_Demo_01/src/com/dxc/service/PersonService.java
@@ -0,0 +1,17 @@
+package com.dxc.service;
+
+import com.dxc.model.Person;
+
+public class PersonService {
+	
+	Person person;
+	
+	public Person getPerson() {
+		return person;
+	}
+	
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+}
