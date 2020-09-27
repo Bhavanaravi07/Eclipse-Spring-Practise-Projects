020a56a8edd6f3f4fb71ac8c6b0042374ed16ab5
bravichandr2 <bravichandr2@host.docker.internal>
2020-09-27 11:39:41 +0530

    commit

---
+++ demo_07_MongoDB/src/main/java/com/dxc/demo/Demo07MongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.dxc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo07MongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo07MongoDbApplication.class, args);
+	}
+
+}
